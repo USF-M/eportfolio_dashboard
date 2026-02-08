fc4b255958b1d2f47d0071d71c683f56fc04b940
Youssef <8957457@alu.murciaeduca.es>
2026-02-08 15:53:35 +0100

    DWEC UT9 - ePortfolio - 1.e + f

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -5,12 +5,13 @@ import Typography from '@mui/material/Typography';
 //import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
 import useRoles from '../hooks/useRoles';
+import opcionesMenu from '../mocks/mock-administrador';
 
 export default function Menu() {
     const { lista } = useRoles();
     function generarAccordionRol(rol) {
         return (
-            <Accordion>
+            <Accordion key={rol}>
                 <AccordionSummary
                     expandIcon="^"
                     aria-controls="panel1-content"
@@ -18,10 +19,29 @@ export default function Menu() {
                 >
                     <Typography component="span">{rol}</Typography>
                 </AccordionSummary>
+
                 <AccordionDetails>
+                    {generarMenu(rol)}
                 </AccordionDetails>
+
             </Accordion>
         )
+    }
+    function generarMenu(rol) {
+        switch (rol) {
+            case 'administrador':
+                return opcionesMenu?.administrador?.map((item) => (
+                    <div key={item.ruta}>
+                        <Button href={item.ruta}>{item.nombre}</Button>
+                    </div>
+                ));
+            case 'docente':
+                return "docente";
+            case 'alumno':
+                return "alumno";
+            default:
+                return "null";
+        }
     }
     return (
         <div>
